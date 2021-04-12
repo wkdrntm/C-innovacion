8bf687f0ea396b76d08ed7e4141991fec73f5943
root <wkdrmtm@naver.com>
2021-04-12 23:05:07 +0900

    C-Innovacion first commit

---
+++ js/ui.js
@@ -0,0 +1,52 @@
+
+$('header nav .menu-item > a').on('mouseenter',function (){
+	// $('header nav .menu-item > a').removeClass('on');
+	$(this).addClass('on');
+})
+
+$('header nav .menu-item').on('mouseleave',function (){
+	$('header nav .menu-item > a').removeClass('on');
+p	// $('nav.gnb').removeClass('on');
+
+})
+
+// 모바일 메뉴 열기
+function mobileMenuOpen() {
+	$('.container').addClass('overlay')
+
+	// var h = $(document).height();
+	// alert(h)
+	// $('nav.mobile-menu').css({"height":h})
+	$('nav.mobile-menu').show("slide", {direction: "right"});
+
+	scrollDisable();
+}
+
+$('.depth1 .depth1-txt').on('click',function (){
+
+	if($(this).hasClass('on')){
+		$(this).removeClass('on');
+	}else{
+		$('.depth1 .depth1-txt').removeClass('on');
+		$(this).addClass('on');
+	}
+
+})
+
+// 모바일 메뉴 닫기
+function mobileMenuClose() {
+
+	$('nav.mobile-menu').hide("slide", {direction: "right"});
+	$('.container').removeClass('overlay');
+	scrollAble();
+}
+
+
+function scrollDisable(){
+	$('html, body').addClass('scrollHidden').on('scroll touchmove mousewheel', function(e){
+		e.preventDefault();
+	});
+}
+function scrollAble(){
+	$('html, body').removeClass('scrollHidden');
+}
